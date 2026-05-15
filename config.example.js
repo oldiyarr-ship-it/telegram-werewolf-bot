c31adfa41fdb5f4818ce21e3b621a2ecc64878a6
oldiyarr-ship-it <oldiyarr@gmail.com>
2026-05-15 13:33:31 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 var config = {
-  "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+  "token": "8300477501:AAHFR-tNYaV8wnc3fMn9wdLAW1fPEbocDYE",
   "update_type": "long-polling", // or "webhook"
   "webhook": "https://www.example.com/<token>",
   //"proxy": "http://127.0.0.1:8118",
